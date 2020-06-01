8c1b33a9d3b30578ed52af9f113ba9c51f7ede85
松本 <matsumoto@giftpad.jp>
2020-06-01 16:59:55 +0900

    generate先のフォルダ変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,71 +1,64 @@
-import pkg from './package'
-const webpack = require('webpack')
+import pkg from "./package";
+const webpack = require("webpack");
 
 export default {
   generate: {
-    fallback: true
+    fallback: true,
+    dir: "docs"
   },
 
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    titleTemplate: '%s | ポートフォリオ',
+    titleTemplate: "%s | ポートフォリオ",
     meta: [
-      { charset: 'utf-8' },
-      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'ポートフォリオ' },
-      { name: 'keywords', content: 'ポートフォリオ,実績' },
-      { name: 'robots', content: 'noindex' }
+      { charset: "utf-8" },
+      { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "ポートフォリオ" },
+      { name: "keywords", content: "ポートフォリオ,実績" },
+      { name: "robots", content: "noindex" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/portfolio/favicon.ico' }
-    ],
+      { rel: "icon", type: "image/x-icon", href: "/portfolio/favicon.ico" }
+    ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   // ここから
   build: {
-    vendor: [
-      'gsap'
-    ]
+    vendor: ["gsap"]
   },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/css/style.css'
-  ],
+   ** Global CSS
+   */
+  css: ["~/assets/css/style.css"],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~/plugins/mixin'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["~/plugins/mixin"],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    publicPath: '/static/',
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    publicPath: "/static/",
+    extend(config, ctx) {}
   },
   router: {
-    base: '/portfolio/'
+    base: "/portfolio/"
   }
-}
+};
